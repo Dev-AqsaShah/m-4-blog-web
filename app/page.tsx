d515deb2c04a028764fdd64e3d616acabe5d9bc7
aqsashah <aqsanor665@gmail.com>
2025-07-20 23:03:22 +0500

    .

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,15 @@ import Header from "@/Components/Header"
 import BlogList from "@/Components/BlogList"
 import Footer from "@/Components/Footer"
 import { ToastContainer } from "react-toastify"
-import "react-toastify/dist/ReactToastify.css";
+import "react-toastify/dist/ReactToastify.css"
+import BlogItem from "@/Components/BlogItem"
 
 export default function Home() {
   return (
     <>
     <ToastContainer theme="dark"/>
     <Header />
+    <BlogItem id={""} image={""} title={""} description={""} category={""} author={""}/>
     <BlogList />
     <Footer />
     </>
